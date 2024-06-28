924434b4f2d4dd3e3acae940b9dd455603af7ccb
Dea Sharma <157221176+DeaSharma@users.noreply.github.com>
2024-06-29 02:36:25 +0530

    Update gptScriptInstance.ts

--- lib/gptScriptInstance.ts
+++ lib/gptScriptInstance.ts
@@ -1,7 +1,7 @@
 import { GPTScript } from "@gptscript-ai/gptscript";
 
 const g = new GPTScript({
-    APIKey: process.env.OPEN_API_KEY,
+    APIKey: process.env.OPENAI_API_KEY,
 });
 
 export default g;
